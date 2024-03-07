6f057ca00e4aea88c1151fcc6b707d7af55beaad
sergei <sergei@s.s>
2024-03-07 12:37:53 +0300

    добавил комментарий

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-0# This is a sample Python script.
-
+# This is a sample Python script.
+#dfgbdfgdfgdf
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
